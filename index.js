a20bd9d4082b7824f9e3c433be5548577f405422
aadarsh4u-code <aadarshkushwaha0208@gmail.com>
2021-01-27 21:20:50 +0545

    multiple reducer is created and combine with combineReducers

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
 // import {redux} from 'redux'; //redux is imported
-const redux = require('redux');
+const redux = require("redux");
 
 const createStore = redux.createStore; //store is created
+const combineReducers = redux.combineReducers; // combine reducer is created form multiple reducer
 
-const Buy_Laptop = 'Buy_Laptop'; // action type is define
-const Buy_Camera = 'Buy_Camera';
- //initial state of app is define
-const initialState = {
-  numberOfLaptop : 15,
-  numberOfCamera : 20
-}
+const Buy_Laptop = "Buy_Laptop"; // action type is define
+const Buy_Camera = "Buy_Camera";
+//initial state of app is define
+const initialStateLaptop = {
+  numberOfLaptop: 15,
+};
+
+const initialStateCamera = {
+  numberOfCamera: 20,
+};
 
 // const action = {
 //   type: Buy_Laptop,
 //   payload: 'My first redux app'
 // }
 
-
 //action function or Action creator => wrappig the action in a single function
 function buyLaptop() {
   // return action; // or we can also write like given below
   return {
-      type: Buy_Laptop,
-      payload: 'My first action code'
-    }
+    type: Buy_Laptop,
+    payload: "My first action code",
+  };
 }
 
 function buyCamera() {
   return {
     type: Buy_Camera,
-    payload: 'My second action'
-  }
+    payload: "My second action",
+  };
 }
 
-//reducer work is to take (prevState, action) => newState 
-const Reducer = (state=initialState, action) => {
-  switch(action.type) {
-    case 'Buy_Laptop': return {
-      ...state,
-      numberOfLaptop: state.numberOfLaptop-1
-    }
+//reducer work is to take (prevState, action) => newState
+const laptopReducer = (state = initialStateLaptop, action) => {
+  switch (action.type) {
+    case "Buy_Laptop":
+      return {
+        ...state,
+        numberOfLaptop: state.numberOfLaptop - 1,
+      };
 
-    case 'Buy_Camera' : return {
-      ...state,
-      numberOfCamera: state.numberOfCamera-2
-    }
-
-    default: return state;
+    default:
+      return state;
   }
-}
+};
+
+const cameraReducer = (state = initialStateCamera, action) => {
+  switch (action.type) {
+    case "Buy_Camera":
+      return {
+        ...state,
+        numberOfCamera: state.numberOfCamera - 2,
+      };
+    default:
+      return state;
+  }
+};
+
+const Reducer = combineReducers({
+  laptop: laptopReducer,
+  camera: cameraReducer
+});
 
 //calling a store
 const store = createStore(Reducer);
 console.log("Initial State", store.getState());
-const unsubscribe = store.subscribe(() => console.log('Updated state Value', store.getState()));
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state Value", store.getState())
+);
 store.dispatch(buyLaptop());
 store.dispatch(buyLaptop());
 store.dispatch(buyLaptop());
