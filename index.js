faac55958ece32d83f8ae9ea4bf790d0758f4a46
aadarsh4u-code <aadarshkushwaha0208@gmail.com>
2021-01-27 20:24:30 +0545

    multiple action

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const redux = require('redux');
 const createStore = redux.createStore; //store is created
 
 const Buy_Laptop = 'Buy_Laptop'; // action type is define
-
+const Buy_Camera = 'Buy_Camera';
  //initial state of app is define
 const initialState = {
-  numberOfLaptop : 15
+  numberOfLaptop : 15,
+  numberOfCamera : 20
 }
 
 // const action = {
@@ -16,13 +17,20 @@ const initialState = {
 // }
 
 
-//action function is define where action is returned
+//action function or Action creator => wrappig the action in a single function
 function buyLaptop() {
   // return action; // or we can also write like given below
   return {
       type: Buy_Laptop,
-      payload: 'My first redux app'
+      payload: 'My first action code'
     }
+}
+
+function buyCamera() {
+  return {
+    type: Buy_Camera,
+    payload: 'My second action'
+  }
 }
 
 //reducer work is to take (prevState, action) => newState 
@@ -32,6 +40,12 @@ const Reducer = (state=initialState, action) => {
       ...state,
       numberOfLaptop: state.numberOfLaptop-1
     }
+
+    case 'Buy_Camera' : return {
+      ...state,
+      numberOfCamera: state.numberOfCamera-2
+    }
+
     default: return state;
   }
 }
@@ -43,4 +57,7 @@ const unsubscribe = store.subscribe(() => console.log('Updated state Value', sto
 store.dispatch(buyLaptop());
 store.dispatch(buyLaptop());
 store.dispatch(buyLaptop());
+store.dispatch(buyCamera());
+store.dispatch(buyCamera());
+store.dispatch(buyCamera());
 unsubscribe();
